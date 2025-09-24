db5cc4411f3d6ffb480b34c4bf6544ae7b2b791d
Andrei Zubov <azubov@gitlab.com>
2025-09-24 16:44:57 +0200

    add colors and tabs styling to wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -19,6 +19,10 @@ config.harfbuzz_features = { 'calt=1', 'clig=1', 'liga=1' }
 -- Optional: Color scheme
 config.color_scheme = 'Tomorrow Night'
 
+config.colors = {
+	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+}
+
 -- Optional: Window settings
 config.window_background_opacity = 1.0
 config.window_decorations = "RESIZE"
@@ -49,6 +53,45 @@ local keys = {
 }
 
 config.keys = keys
+
+
+-- Tab title formatting
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.tab_max_width = 999
+
+function get_max_cols(window)
+  local tab = window:active_tab()
+  local cols = tab:get_size().cols
+  return cols
+end
+
+wezterm.on(
+  'window-config-reloaded',
+  function(window)
+    wezterm.GLOBAL.cols = get_max_cols(window)
+  end
+)
+
+wezterm.on(
+  'window-resized',
+  function(window, pane)
+    wezterm.GLOBAL.cols = get_max_cols(window)
+  end
+)
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local title = tab.active_pane.title
+    local full_title = '[' .. tab.tab_index + 1 .. '] ' .. title
+    local pad_length = (wezterm.GLOBAL.cols // #tabs - #full_title) // 2
+    if pad_length * 2 + #full_title > max_width then
+      pad_length = (max_width - #full_title) // 2
+    end
+    return string.rep(' ', pad_length) .. full_title .. string.rep(' ', pad_length)
+  end
+)
 
 
 return config
